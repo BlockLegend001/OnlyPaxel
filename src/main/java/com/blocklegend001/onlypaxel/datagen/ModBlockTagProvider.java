d9c67336b99308fda9ece9a4ea72df931d27cfc5
BlockLegend001 <rosarioarcamone@gmail.com>
2025-06-29 21:42:08 +0200

    Added 1.21.6-Forge

---
+++ src/main/java/com/blocklegend001/onlypaxel/datagen/ModBlockTagProvider.java
@@ -0,0 +1,26 @@
+package com.blocklegend001.onlypaxel.datagen;
+
+import com.blocklegend001.onlypaxel.OnlyPaxel;
+import com.blocklegend001.onlypaxel.utils.ModTags;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagProvider extends BlockTagsProvider {
+    public ModBlockTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, OnlyPaxel.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider p_256380_) {
+        tag(ModTags.Blocks.PAXEL_MINEABLE)
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .addTag(BlockTags.MINEABLE_WITH_AXE)
+                .addTag(BlockTags.MINEABLE_WITH_SHOVEL);
+    }
+}
