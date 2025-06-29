2160f7e34235172a23de2b673ae65d8752e2f3fa
BlockLegend001 <rosarioarcamone@gmail.com>
2025-06-29 21:43:07 +0200

    Added 1.21.6-NeoForge

---
+++ src/main/java/com/blocklegend001/onlypaxel/datagen/ModModelProvider.java
@@ -0,0 +1,49 @@
+package com.blocklegend001.onlypaxel.datagen;
+
+import com.blocklegend001.onlypaxel.OnlyPaxel;
+import com.blocklegend001.onlypaxel.item.ModItems;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.ModelProvider;
+import net.minecraft.client.data.models.model.ModelTemplate;
+import net.minecraft.client.data.models.model.ModelTemplates;
+import net.minecraft.client.data.models.model.TextureMapping;
+import net.minecraft.client.data.models.model.TextureSlot;
+import net.minecraft.client.renderer.item.BlockModelWrapper;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.registries.DeferredItem;
+
+import java.util.Collections;
+
+public class ModModelProvider extends ModelProvider {
+    public ModModelProvider(PackOutput output, String modId) {
+        super(output, modId);
+    }
+
+    @Override
+    protected void registerModels(BlockModelGenerators blockModels, ItemModelGenerators itemModels) {
+        itemModel(itemModels, ModItems.WOODEN_PAXEL.get());
+        itemModel(itemModels, ModItems.STONE_PAXEL.get());
+        itemModel(itemModels, ModItems.IRON_PAXEL.get());
+        itemModel(itemModels, ModItems.GOLDEN_PAXEL.get());
+        itemModel(itemModels, ModItems.DIAMOND_PAXEL.get());
+        itemModel(itemModels, ModItems.NETHERITE_PAXEL.get());
+    }
+
+    public void itemModel(ItemModelGenerators itemModels, Item item)
+    {
+        this.itemModel(itemModels, item, ModelTemplates.FLAT_ITEM);
+    }
+
+
+    public void itemModel(ItemModelGenerators itemModels, Item item, ModelTemplate template)
+    {
+        ResourceLocation itemId = BuiltInRegistries.ITEM.getKey(item);
+        ResourceLocation textureLoc = ResourceLocation.fromNamespaceAndPath(itemId.getNamespace(), "item/" + itemId.getPath());
+        TextureMapping textureMapping = new TextureMapping().put(TextureSlot.LAYER0, textureLoc);
+        itemModels.itemModelOutput.accept(item, new BlockModelWrapper.Unbaked(template.create(item, textureMapping, itemModels.modelOutput), Collections.emptyList()));
+    }
+}
