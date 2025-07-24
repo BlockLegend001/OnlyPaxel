5a9ccec6ff734ca0a3d9a1814e80a638ab5696dd
BlockLegend001 <rosarioarcamone@gmail.com>
2025-07-24 11:49:00 +0200

    Added 1.21.8-Forge

---
+++ src/main/java/com/blocklegend001/onlypaxel/utils/ModTags.java
@@ -0,0 +1,19 @@
+package com.blocklegend001.onlypaxel.utils;
+
+import com.blocklegend001.onlypaxel.OnlyPaxel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+
+public class ModTags {
+    public static class Blocks {
+        public static final TagKey<Block> PAXEL_MINEABLE =
+                tag("mineable/paxel_mineable");
+
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(ResourceLocation.fromNamespaceAndPath(OnlyPaxel.MODID, name));
+        }
+    }
+}
