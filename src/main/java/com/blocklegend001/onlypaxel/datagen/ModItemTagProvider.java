bfc7603fc0e14f813f56d27c5f6b5855d08c28ac
BlockLegend001 <rosarioarcamone@gmail.com>
2025-09-12 16:26:30 +0200

    Added more tags

--- src/main/java/com/blocklegend001/onlypaxel/datagen/ModItemTagProvider.java
+++ src/main/java/com/blocklegend001/onlypaxel/datagen/ModItemTagProvider.java
@@ -3,6 +3,7 @@ package com.blocklegend001.onlypaxel.datagen;
 import com.blocklegend001.onlypaxel.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.ItemTags;
 
@@ -65,6 +66,38 @@ public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
                 .add(ModItems.NETHERITE_PAXEL);
 
         valueLookupBuilder(ItemTags.SHOVELS)
+                .add(ModItems.WOODEN_PAXEL)
+                .add(ModItems.STONE_PAXEL)
+                .add(ModItems.IRON_PAXEL)
+                .add(ModItems.GOLDEN_PAXEL)
+                .add(ModItems.DIAMOND_PAXEL)
+                .add(ModItems.NETHERITE_PAXEL);
+
+        valueLookupBuilder(ItemTags.BREAKS_DECORATED_POTS)
+                .add(ModItems.WOODEN_PAXEL)
+                .add(ModItems.STONE_PAXEL)
+                .add(ModItems.IRON_PAXEL)
+                .add(ModItems.GOLDEN_PAXEL)
+                .add(ModItems.DIAMOND_PAXEL)
+                .add(ModItems.NETHERITE_PAXEL);
+
+        valueLookupBuilder(ConventionalItemTags.TOOLS)
+                .add(ModItems.WOODEN_PAXEL)
+                .add(ModItems.STONE_PAXEL)
+                .add(ModItems.IRON_PAXEL)
+                .add(ModItems.GOLDEN_PAXEL)
+                .add(ModItems.DIAMOND_PAXEL)
+                .add(ModItems.NETHERITE_PAXEL);
+
+        valueLookupBuilder(ItemTags.CLUSTER_MAX_HARVESTABLES)
+                .add(ModItems.WOODEN_PAXEL)
+                .add(ModItems.STONE_PAXEL)
+                .add(ModItems.IRON_PAXEL)
+                .add(ModItems.GOLDEN_PAXEL)
+                .add(ModItems.DIAMOND_PAXEL)
+                .add(ModItems.NETHERITE_PAXEL);
+
+        valueLookupBuilder(ConventionalItemTags.MINING_TOOL_TOOLS)
                 .add(ModItems.WOODEN_PAXEL)
                 .add(ModItems.STONE_PAXEL)
                 .add(ModItems.IRON_PAXEL)
