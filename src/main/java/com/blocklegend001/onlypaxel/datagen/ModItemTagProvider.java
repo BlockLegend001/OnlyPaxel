6c6f664662ca212fcfd2b5d2043712a7490157d5
BlockLegend001 <rosarioarcamone@gmail.com>
2025-10-07 22:31:40 +0200

    Added 1.21.9-NeoForge

---
+++ src/main/java/com/blocklegend001/onlypaxel/datagen/ModItemTagProvider.java
@@ -0,0 +1,109 @@
+package com.blocklegend001.onlypaxel.datagen;
+
+import com.blocklegend001.onlypaxel.OnlyPaxel;
+import com.blocklegend001.onlypaxel.item.ModItems;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.VanillaItemTagsProvider;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.common.Tags;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends VanillaItemTagsProvider {
+    public ModItemTagProvider(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> completableFuture) {
+        super(packOutput, completableFuture);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+        tag(ItemTags.DURABILITY_ENCHANTABLE)
+                .add(ModItems.WOODEN_PAXEL.get())
+                .add(ModItems.STONE_PAXEL.get())
+                .add(ModItems.IRON_PAXEL.get())
+                .add(ModItems.GOLDEN_PAXEL.get())
+                .add(ModItems.DIAMOND_PAXEL.get())
+                .add(ModItems.NETHERITE_PAXEL.get());
+
+        tag(ItemTags.MINING_ENCHANTABLE)
+                .add(ModItems.WOODEN_PAXEL.get())
+                .add(ModItems.STONE_PAXEL.get())
+                .add(ModItems.IRON_PAXEL.get())
+                .add(ModItems.GOLDEN_PAXEL.get())
+                .add(ModItems.DIAMOND_PAXEL.get())
+                .add(ModItems.NETHERITE_PAXEL.get());
+
+        tag(ItemTags.MINING_LOOT_ENCHANTABLE)
+                .add(ModItems.WOODEN_PAXEL.get())
+                .add(ModItems.STONE_PAXEL.get())
+                .add(ModItems.IRON_PAXEL.get())
+                .add(ModItems.GOLDEN_PAXEL.get())
+                .add(ModItems.DIAMOND_PAXEL.get())
+                .add(ModItems.NETHERITE_PAXEL.get());
+
+        tag(ItemTags.VANISHING_ENCHANTABLE)
+                .add(ModItems.WOODEN_PAXEL.get())
+                .add(ModItems.STONE_PAXEL.get())
+                .add(ModItems.IRON_PAXEL.get())
+                .add(ModItems.GOLDEN_PAXEL.get())
+                .add(ModItems.DIAMOND_PAXEL.get())
+                .add(ModItems.NETHERITE_PAXEL.get());
+
+        tag(ItemTags.AXES)
+                .add(ModItems.WOODEN_PAXEL.get())
+                .add(ModItems.STONE_PAXEL.get())
+                .add(ModItems.IRON_PAXEL.get())
+                .add(ModItems.GOLDEN_PAXEL.get())
+                .add(ModItems.DIAMOND_PAXEL.get())
+                .add(ModItems.NETHERITE_PAXEL.get());
+
+        tag(ItemTags.PICKAXES)
+                .add(ModItems.WOODEN_PAXEL.get())
+                .add(ModItems.STONE_PAXEL.get())
+                .add(ModItems.IRON_PAXEL.get())
+                .add(ModItems.GOLDEN_PAXEL.get())
+                .add(ModItems.DIAMOND_PAXEL.get())
+                .add(ModItems.NETHERITE_PAXEL.get());
+
+        tag(ItemTags.SHOVELS)
+                .add(ModItems.WOODEN_PAXEL.get())
+                .add(ModItems.STONE_PAXEL.get())
+                .add(ModItems.IRON_PAXEL.get())
+                .add(ModItems.GOLDEN_PAXEL.get())
+                .add(ModItems.DIAMOND_PAXEL.get())
+                .add(ModItems.NETHERITE_PAXEL.get());
+
+        tag(ItemTags.CLUSTER_MAX_HARVESTABLES)
+                .add(ModItems.WOODEN_PAXEL.get())
+                .add(ModItems.STONE_PAXEL.get())
+                .add(ModItems.IRON_PAXEL.get())
+                .add(ModItems.GOLDEN_PAXEL.get())
+                .add(ModItems.DIAMOND_PAXEL.get())
+                .add(ModItems.NETHERITE_PAXEL.get());
+
+        tag(ItemTags.BREAKS_DECORATED_POTS)
+                .add(ModItems.WOODEN_PAXEL.get())
+                .add(ModItems.STONE_PAXEL.get())
+                .add(ModItems.IRON_PAXEL.get())
+                .add(ModItems.GOLDEN_PAXEL.get())
+                .add(ModItems.DIAMOND_PAXEL.get())
+                .add(ModItems.NETHERITE_PAXEL.get());
+
+        tag(Tags.Items.TOOLS)
+                .add(ModItems.WOODEN_PAXEL.get())
+                .add(ModItems.STONE_PAXEL.get())
+                .add(ModItems.IRON_PAXEL.get())
+                .add(ModItems.GOLDEN_PAXEL.get())
+                .add(ModItems.DIAMOND_PAXEL.get())
+                .add(ModItems.NETHERITE_PAXEL.get());
+
+        tag(Tags.Items.MINING_TOOL_TOOLS)
+                .add(ModItems.WOODEN_PAXEL.get())
+                .add(ModItems.STONE_PAXEL.get())
+                .add(ModItems.IRON_PAXEL.get())
+                .add(ModItems.GOLDEN_PAXEL.get())
+                .add(ModItems.DIAMOND_PAXEL.get())
+                .add(ModItems.NETHERITE_PAXEL.get());
+    }
+}
